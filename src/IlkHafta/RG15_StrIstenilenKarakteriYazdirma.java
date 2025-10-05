e23df0d92be291588b8f4751147418f9b5d53995
melosnur <melahatnurozcan@gmail.com>
2025-10-05 21:38:23 +0300

    fix error

--- src/IlkHafta/RG15_StrIstenilenKarakteriYazdirma.java
+++ src/IlkHafta/RG15_StrIstenilenKarakteriYazdirma.java
@@ -9,9 +9,9 @@ public class RG15_StrIstenilenKarakteriYazdirma {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Girmek istediginiz cumle veya kelimeyi giriniz.");
         String str = scanner.nextLine();
-        System.out.println("Girdiginiz kelimenin veya cumlenin istediginiz siradaki harfin sirasini girin");
+        System.out.println("Girdiginiz kelimenin veya cumleninme istediginiz siradaki karakter sirasini girin");
         int sira = scanner.nextInt();
 
-        System.out.println(sira + ". siradaki harf : " +  str.substring(sira-1,sira));
+        System.out.println(sira + ". siradaki karakter : " +  str.substring(sira-1,sira));
     }
 }
